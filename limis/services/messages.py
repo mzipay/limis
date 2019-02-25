27db40a6cc5ea35781ea51337b4d5e061e440705
Philip Streck <philip.streck@gmail.com>
2019-02-24 19:09:52 -0500

    services added

---
+++ limis/services/messages.py
@@ -0,0 +1,12 @@
+COMPONENT_ACTION_NOT_IMPLEMENTED_ERROR = '"{}" action is not implemented for this service.'
+RESOURCE_DESERIALIZE_OBJECT_TYPE_MISMATCH = '"{}" attribute has type "{}" which does not match the object type "{}".'
+RESOURCE_DESERIALIZE_OBJECT_MISSING_ATTRIBUTE = '"{}" does not have attribute "{}" defined.'
+RESOURCE_DESERIALIZE_INVALID_DATA_TYPE = 'Data must be a JSON object.'
+RESOURCE_SERIALIZE_UNKNOWN_ATTRIBUTE = 'Attribute "{}" is not present in class "{}".'
+SERVICE_ADD_COMPONENT = 'Service "{}" registering component "{}" with path "{}".'
+SERVICE_ADD_COMPONENT_INVALID_COMPONENT_CLASS = 'Component "{}" is not a subclass of Component class.'
+SERVICE_ADD_COMPONENT_REGISTER_HANDLER = 'Service "{}" registering component "{}" handler "{}".'
+SERVICE_ADD_COMPONENT_WITH_NO_HANDLER = 'Component "{}" has been registered with the Service "{}" but \
+does not have any handlers defined.'
+SERVICE_INIT_REGISTER_COMPONENTS_STARTED = 'Service "{}" registering components.'
+SERVICE_INIT_REGISTER_COMPONENTS_COMPLETED = 'Service "{}" registered components.'
